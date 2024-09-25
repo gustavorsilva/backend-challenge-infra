0da0b2025d1c71dd3ef55e6e780e3a7af8c8e5ce
root <root@GustavoLigon.localdomain>
2024-09-25 12:59:30 -0300

    opentofu

--- main.tf
+++ main.tf
@@ -1,60 +1,20 @@
-resource "aws_eks_cluster" "this" {
-  name     = var.cluster_name
-  role_arn = var.cluster_role_arn
-
-  vpc_config {
-    subnet_ids         = var.subnet_ids
-    security_group_ids = [aws_security_group.eks_cluster.id]
-  }
-
-  tags = {
-    Name = var.cluster_name
-  }
+# Configuração do provedor AWS
+provider "aws" {
+  region = "us-east-1"  # Substitua pela sua região desejada, ex: "sa-east-1" para São Paulo
 }
 
-resource "aws_security_group" "eks_cluster" {
-  name        = "${var.cluster_name}-sg"
-  description = "Security group for EKS cluster"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    description = "Allow internal traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Criação do repositório ECR
+resource "aws_ecr_repository" "meu_repositorio_ecr" {
+  name                 = "aplication-backend-challenge"
+  image_tag_mutability = "MUTABLE"  # Opções: "MUTABLE" ou "IMMUTABLE"
 
   tags = {
-    Name = "${var.cluster_name}-sg"
-  }
+    Projeto     = "backend-challenge"
+	}
 }
 
-resource "aws_eks_node_group" "this" {
-  cluster_name    = aws_eks_cluster.this.name
-  node_group_name = "${var.cluster_name}-node-group"
-  node_role_arn   = var.node_role_arn
-  subnet_ids      = var.subnet_ids
-
-  scaling_config {
-    desired_size = var.desired_capacity
-    max_size     = var.max_size
-    min_size     = var.min_size
-  }
-
-  instance_types = var.instance_types
-  capacity_type = var.capacity_type
-
-  tags = {
-    Name = "${var.cluster_name}-node-group"
-  }
-
-  depends_on = [aws_eks_cluster.this]
+# Saída do URI do repositório
+output "ecr_repository_uri" {
+  description = "URI do repositório ECR criado"
+  value       = aws_ecr_repository.meu_repositorio_ecr.repository_url
 }
